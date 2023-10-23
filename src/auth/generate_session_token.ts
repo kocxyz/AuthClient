aee4e0c2512005206cd4aef0648fb739eaeb847a
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-10-23 23:32:45 +0200

    fix: Type for `generateSessionToken`

--- src/auth/generate_session_token.ts
+++ src/auth/generate_session_token.ts
@@ -5,23 +5,23 @@ export type GenerateSessionTokenResponse = {
   /**
    * The session token that can be used to authenticate
    * against the private servers auth proxy.
-   * 
+   *
    * This has to be passed as username to the client
    * when launching.
    */
-  authToken: string;
+  authkey: string;
 };
 
 /**
  * Generate a session token for authentication again the auth proxy.
- * 
+ *
  * **Note**: The retrieved token is only valid once and expires 10 minutes.
- * 
+ *
  * @param baseUrl The Auth Server base URL.
  * @param username The username of the account.
  * @param authToken The authToken of the account.
  * @param server The server to create the session token for.
- * 
+ *
  * @returns The created session token.
  */
 export function generateSessionToken(baseUrl: string, username: string, authToken: string, server: KOCServerUrl) {
